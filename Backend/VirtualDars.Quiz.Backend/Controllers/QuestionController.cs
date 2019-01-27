bdb0c5ef171f4f4c6e745943ae0ba356171f3708
Farkhod Dadajanov <dfarkhod@gmail.com>
2019-01-27 13:07:41 +0800

    Created QuestionController & model.

---
+++ Backend/VirtualDars.Quiz.Backend/Controllers/QuestionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VirtualDars.Quiz.Backend.Models;
+
+namespace VirtualDars.Quiz.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] Question quesion)
+        {
+        }
+    }
+}
---
+++ Backend/VirtualDars.Quiz.Backend/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualDars.Quiz.Backend.Models
+{
+    public class Question
+    {
+        public string Text { get; set; }
+    }
+}
