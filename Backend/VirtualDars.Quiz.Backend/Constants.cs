0a6ae7dd156672cff5afd8b7f6eeab7c9f240866
Farkhod Dadajanov <dfarkhod@gmail.com>
2019-05-20 14:33:57 +0800

    added missing file

---
+++ Backend/VirtualDars.Quiz.Backend/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualDars.Quiz.Backend
+{
+    public static class Constants
+    {
+        public const string KEY_PHRASE = "bU siRl1 K@l1t s0Z!";
+    }
+}
